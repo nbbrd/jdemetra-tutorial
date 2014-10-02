b686274b1fa870ba97f0e8e7ca8cb60bb60ca20d
Philippe Charles <philippe.charles@nbb.be>
2014-10-02 14:24:01 +0000

    Removed useless resources.
    git-svn-id: http://srvdqrd2.lanprd.nbb:8008/svn/jdemetra_tuto/trunk@29 f8fc5b52-ec4d-734a-90eb-239e487896a7

--- tutorial-plugin-dstats/src/main/java/be/nbb/demetra/dstats/DStatsHelper.java
+++ tutorial-plugin-dstats/src/main/java/be/nbb/demetra/dstats/DStatsHelper.java
@@ -17,12 +17,13 @@
 package be.nbb.demetra.dstats;
 
 import com.google.common.collect.Lists;
+import ec.util.various.swing.FontAwesome;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.util.List;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import org.netbeans.api.options.OptionsDisplayer;
-import org.openide.util.ImageUtilities;
 import org.openide.windows.IOProvider;
 import org.openide.windows.InputOutput;
 
@@ -38,7 +39,8 @@ final class DStatsHelper {
     private static final class OpenSettings extends AbstractAction {
 
         public OpenSettings() {
-            putValue(SMALL_ICON, ImageUtilities.loadImageIcon("/be/nbb/demetra/dstats/preferences-system.png", false));
+            putValue(SMALL_ICON, FontAwesome.FA_SLIDERS.getIcon(Color.BLACK, 14f));
+            putValue(SHORT_DESCRIPTION, "Open settings");
         }
 
         @Override
